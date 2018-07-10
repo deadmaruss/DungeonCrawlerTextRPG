cd3900002c41bb60b0966403d69a5701d868ee0d
Slavik <deadmaruss@gmail.com>
2018-07-10 12:00:11 +0300

    Add project files.

---
+++ DungeonCrawler/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DungeonCrawler.Strings;
+
+namespace DungeonCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MillisecondsTimeout = 2000;
+
+            int PlayerHealth = 100;
+            int PlayerAttack = 5;
+            int InventorySpace = 12;
+            int Sneak = 1;
+            Random rnd = new Random();
+
+            Console.WriteLine(PRISON01);
+            System.Threading.Thread.Sleep(MillisecondsTimeout);
+            Console.WriteLine(PRISON02);
+            System.Threading.Thread.Sleep(MillisecondsTimeout);
+            Console.WriteLine(PRISON03);
+            System.Threading.Thread.Sleep(MillisecondsTimeout);
+            string PlayerChoice01 = Console.ReadLine();
+            bool HavePrisonKey = false;
+            switch (PlayerChoice01)
+            {
+                case "y":
+                    Console.WriteLine(CHOICE01Y);
+                    HavePrisonKey = true;
+                    InventorySpace--;
+                    System.Threading.Thread.Sleep(MillisecondsTimeout);
+                    break;
+                case "n":
+                    Console.WriteLine(CHOICE01N);
+                    System.Threading.Thread.Sleep(MillisecondsTimeout);
+                    break;
+                default:
+                    Console.WriteLine(CHOICE01I);
+                    System.Threading.Thread.Sleep(MillisecondsTimeout);
+                    break;
+            }
+            Console.WriteLine(PRISON04);
+            System.Threading.Thread.Sleep(MillisecondsTimeout);
+            Console.WriteLine(PRISON05);
+            System.Threading.Thread.Sleep(MillisecondsTimeout);
+            Console.WriteLine(PRISON06);
+            System.Threading.Thread.Sleep(MillisecondsTimeout);
+            Console.WriteLine(PRISON07);
+            System.Threading.Thread.Sleep(MillisecondsTimeout);
+            string PlayerChoice02 = Console.ReadLine();
+            switch (PlayerChoice02)
+            {
+                case "y":
+                    Console.WriteLine(CHOICE02Y);
+                    System.Threading.Thread.Sleep(MillisecondsTimeout);
+                    int dice = rnd.Next(Sneak, 8);
+                    if (dice <= 4)
+                    {
+                        Console.WriteLine(STATCHKF);
+                        System.Threading.Thread.Sleep(MillisecondsTimeout);
+                        EnemyNPC goblin1 = new EnemyNPC
+                        {
+                            NPCID = 001,
+                            NPCName = "Goblin Thug",
+                            NPCHealth = 10,
+                            NPCAttack = 10
+                        };
+                        Console.WriteLine(goblin1.Encounter());
+                        while (goblin1.NPCHealth > 0 && PlayerHealth > 0)
+                        {
+                            int Attack = rnd.Next(0, 5);
+                            if (Attack > 3)
+                            {
+                                goblin1.NPCHealth = goblin1.NPCHealth - PlayerAttack;
+                                Console.WriteLine("You hit");
+                                Console.WriteLine(ENEMYHP + goblin1.NPCHealth);
+                                Console.WriteLine(PLAYERHP + PlayerHealth);
+                            }
+                            else
+                            {
+                                PlayerHealth = PlayerHealth - goblin1.NPCAttack;
+                                Console.WriteLine(goblin1.NPCName + " hits");
+                                Console.WriteLine(PLAYERHP + PlayerHealth);
+                                Console.WriteLine(ENEMYHP + goblin1.NPCHealth);
+                            }
+                            System.Threading.Thread.Sleep(MillisecondsTimeout);
+
+                        }
+                        if (goblin1.NPCHealth == 0)
+                        {
+                            Console.WriteLine(VICTORY);
+                            System.Threading.Thread.Sleep(MillisecondsTimeout);
+                        }
+                        else
+                        {
+                            Console.WriteLine(DEFEAT);
+                            System.Threading.Thread.Sleep(MillisecondsTimeout);
+                        }
+                    } else
+                    {
+                        Console.WriteLine(STATCHKS);
+                        Sneak++;
+                        System.Threading.Thread.Sleep(MillisecondsTimeout);
+                    }
+                    break;
+                case "n":
+                    Console.WriteLine(CHOICE02N);
+                    System.Threading.Thread.Sleep(MillisecondsTimeout);
+                    break;
+                default:
+                    Console.WriteLine(CHOICE02I);
+                    System.Threading.Thread.Sleep(MillisecondsTimeout);
+                    break;
+            }
+            Console.WriteLine(PRISON08);
+            System.Threading.Thread.Sleep(MillisecondsTimeout);
+            Console.WriteLine(PRISON09);
+            System.Threading.Thread.Sleep(MillisecondsTimeout);
+            if (HavePrisonKey == true)
+            {
+                Console.WriteLine(PRISON10);
+                System.Threading.Thread.Sleep(MillisecondsTimeout);
+            }
+            else
+            {
+                Console.WriteLine(PRISON11);
+                System.Threading.Thread.Sleep(MillisecondsTimeout);
+                Console.WriteLine(FIELD01);
+            }
+        }
+    }
+}
+
+public class EnemyNPC
+{
+    public EnemyNPC() { }
+    public EnemyNPC(int id, string name, int hp, int attack)
+    {
+        NPCID = id;
+        NPCName = name;
+        NPCHealth = hp;
+        NPCAttack = attack;
+    }
+
+    public int NPCID { get; set;  }
+    public string NPCName { get; set; }
+    public int NPCHealth { get; set; }
+    public int NPCAttack { get; set; }
+
+    public string Encounter()
+    {
+        return NPCName + ", " + NPCHealth + " HP, " + NPCAttack + " Attack";
+    }
+}
---
+++ DungeonCrawler/strings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace DungeonCrawler
+{
+    public class Strings
+    {
+        public static string VICTORY = "You Win!";
+        public static string DEFEAT = "You lose..";
+
+        public static string ENEMYHP = "Enemy HP: ";
+        public static string PLAYERHP = "Your HP: ";
+
+
+        public static string PRISON01 = "You are standing in a long hallway";
+        public static string PRISON02 = "You see a key on the floor";
+        public static string PRISON03 = "Do you want to pick it up? (y/n)";
+        public static string PRISON04 = "In the end of the hallway you see a bright light";
+        public static string PRISON05 = "Going towards it you encounter a goblin";
+        public static string PRISON06 = "He doesn't seem to notice you yet";
+        public static string PRISON07 = "Do you want to fight it? (y/n)";
+        public static string PRISON08 = "You see a door";
+        public static string PRISON09 = "It is locked";
+        public static string PRISON10 = "You remember that you found a key earlier and you try to see if it fits. Unsurprisingly for you, it does. The door is open and you are free";
+        public static string PRISON11 = "Seems like you need some kind of a key to open it. Maybe the one behind would fit?";
+
+        public static string FIELD01 = "Vast field lays in frond of you";
+
+        public static string CHOICE01Y = "You picked up the key";
+        public static string CHOICE01N = "You decided to leave the key be";
+        public static string CHOICE01I = "You are unsure what to do so you leave the key laying there. Maybe later you could come back and pick it up if you need...";
+
+        public static string CHOICE02Y = "You try to sneak up to it, and...";
+        public static string CHOICE02N = "You don't want to fight so you try to come up with a way to get past it";
+        public static string CHOICE02I = "While you are standing there thinking it noticed you and you won't get away without a fight anymore";
+
+        public static string BATTLE01;
+        public static string BATTLE02;
+        public static string BATTLE03;
+        public static string BATTLE04;
+        public static string BATTLE05;
+
+        public static string STATCHK1;
+        public static string STATCHK2;
+        public static string STATCHKS = "You successfully sneak up behind it and dealt a killing blow";
+        public static string STATCHKF = "Unfortunately, he noticed you and you start a fight";
+    }
+}
